83e4867561afd2bee81290731739d5e43ec6faf1
yuzuki <nanami2962@gmail.com>
2022-06-23 09:13:55 +0800

    add a lot of config

--- lve_pipeline.h
+++ lve_pipeline.h
@@ -7,7 +7,23 @@
 
 namespace lve
 {
-    struct PipelineConfigInfo {};
+    struct PipelineConfigInfo
+    {
+        VkViewport viewport;
+        VkRect2D scissor;
+        VkPipelineViewportStateCreateInfo viewportInfo;
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo multisampleInfo;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+        VkPipelineLayout pipelineLayout = nullptr;
+        VkRenderPass renderPass = nullptr;
+
+        uint32_t subpass = 0;
+    };
+
 
     class LvePipeline
     {
@@ -16,7 +32,7 @@ namespace lve
                     const std::string& vertexFilePath,
                     const std::string& fragFilePath,
                     const PipelineConfigInfo& ConfigInfo);
-        ~LvePipeline() = default;
+        ~LvePipeline();
         LvePipeline(const LvePipeline&) = delete;
         void operator=(const LvePipeline&) = delete;
 
