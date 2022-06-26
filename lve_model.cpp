d55577d81d6c2f631f8ae290d9204135efded22b
yuzuki <nanami2962@gmail.com>
2022-06-26 09:12:49 +0800

    draw triangle use vertexbuffer

---
+++ lve_model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yuzuki on 2022/6/26.
+//
+
+#include "lve_model.h"
---
+++ lve_model.h
@@ -0,0 +1,14 @@
+//
+// Created by yuzuki on 2022/6/26.
+//
+
+#ifndef LVE_LVE_MODEL_H
+#define LVE_LVE_MODEL_H
+
+
+class lve_model {
+
+};
+
+
+#endif //LVE_LVE_MODEL_H
